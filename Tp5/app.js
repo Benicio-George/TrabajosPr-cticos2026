da02e7832e558865b2acce932469c58655edf9f1
alumno <52590493@ivt.edu.ar>
2026-06-04 09:08:36 -0300

    4 de Junio

--- Tp5/app.js
+++ Tp5/app.js
@@ -1,8 +1,20 @@
-let numero1 = 25;
-let numero2 = 42;
-let resultado
+let suma = 20;
+let resta = 20
+let multiplicacion = 26
+let division = 35
+alert("el resultado de la suma es: " + suma)
+alert("el resultado de la resta es: " + resta)
+alert("el resultado de la multiplicacion es: " + multiplicacion)
+alert("el resultado de la division es: " + division)
 
-resultado = numero1 + numero2
+console.log("el resultado de la suma es: " + suma)
+console.log("el resultado de la resta es: " + resta)
+console.log("el resultado de la multiplicacion es: " + multiplicacion)
+console.log("el resultado de la division es: " + division)
 
-console.log(resultado);
-alert(resultado)
+let nombreAlumno = "Benicio";
+let apellidoAlumno = "George";
+let edadAlumno = 14;
+
+console.log("El alumno: " + nombreAlumno + " " + apellidoAlumno  + " tiene " + edadAlumno + " años" )
+alert("El alumno: " + nombreAlumno + " " + apellidoAlumno  + " tiene " + edadAlumno + " años")
