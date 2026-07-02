487ca96d519b370098675eaa6260077ba1fe87d7
alumno <52590493@ivt.edu.ar>
2026-07-02 09:13:58 -0300

    2 de julio

--- Tp6/app.js
+++ Tp6/app.js
@@ -1,8 +1,10 @@
+let p = document.querySelector('p')
 let botoncambiarfondo = document.querySelector ("#botoncambiarfondo")
 let botoncambiartexto = document.querySelector ("#botoncambiartexto")
 let botoncambiarcolor = document.querySelector ("#botoncambiarcolor")
 let botoncambiarletra = document.querySelector ("#botoncambiarletra")
-let p = document.querySelector('p')
+let botoncambiartamaño = document.querySelector ("#botoncambiartamaño")
+let botonreiniciar = document.querySelector ("#botonreiniciar")
 
 botoncambiarfondo.onclick = function () {
     p.style.backgroundColor = "red" 
@@ -14,4 +16,20 @@ botoncambiartexto.onclick = function () {
 
 botoncambiarcolor.onclick = function () {
     p.style.color = "green"
+}
+
+botoncambiarletra.onclick = function () {
+    p.style.fontFamily = "IMPACT"
+}
+
+botoncambiartamaño.onclick = function () {
+    p.style.fontSize = "30px"
+}
+
+botonreiniciar.onclick = function () {
+    p.style.color = "orange"
+    p.style.backgroundColor = "aqua"
+    p.style.fontSize = "20px"
+    p.textContent = "Primera"
+    p.style.fontFamily = "Arial, Helvetica, sans-serif"
 }
