2f060e4f3d44c2d90ba52abd40e3fe31e933e03a
alumno <52590493@ivt.edu.ar>
2026-07-30 09:14:37 -0300

    30 de julio

---
+++ Tp7/app.js
@@ -0,0 +1,10 @@
+let texto = document.querySelector("texto")
+let btn1 = document.querySelector("btn1")
+let edad = 14
+
+btn1.onclick = function(){
+    texto.textContent = "resultado"
+    if (edad >= 18) {
+        
+    }
+}
