f0b997f4de9fcd25afb06a114abde034408aba7b
alumno <52590493@ivt.edu.ar>
2026-08-06 08:43:36 -0300

    6 de agosto

--- Tp7/app.js
+++ Tp7/app.js
@@ -1,10 +1,24 @@
-let texto = document.querySelector("texto")
-let btn1 = document.querySelector("btn1")
+let texto = document.querySelector("#texto")
+let btn1 = document.querySelector("#btn1")
 let edad = 14
 
 btn1.onclick = function(){
-    texto.textContent = "resultado"
-    if (edad >= 18) {
-        
+texto.textContent = "resultado"
+    if(edad >= 18)
+    {
+        alert("Eres mayor de edad")
+    }else{
+        alert("Eres menor de edad")
+    }
+}
+
+let nombreusuario = "Nahuel"
+let btn2 = document.querySelector("#btn2")
+let nombre = document.querySelector("#nombre")
+
+btn2.onclick = function(){
+    if(nombreusuario = "Nahuel"){nombre.textContent = "Bienvenido Nahuel, ¿cómo estás?"}
+    else{
+        nombre.textContent = "Bienvenido usuario"
     }
 }
