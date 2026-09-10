fa0653c85cd687a7ec915cb9249ff20ec4a94555
alumno <52590493@ivt.edu.ar>
2026-09-10 08:38:40 -0300

    10 de septiembre

---
+++ Tp extra N°2/app.js
@@ -0,0 +1,6 @@
+/*Ejercicio 1*/
+let texto1 = document.querySelector("#texto1")
+let edad = document.querySelector("#edad")
+let entradas = document.querySelector("#entradas")
+let btn1 = document.querySelector("#btn1")
+
